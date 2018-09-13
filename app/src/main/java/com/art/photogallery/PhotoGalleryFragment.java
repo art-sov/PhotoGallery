a1e1b63597413bf257b9b2f7668520eb73578c2a
art-sov <artsovlov@gmail.com>
2018-09-13 12:52:32 +0300

    chapter25 complite

--- app/src/main/java/com/art/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/art/photogallery/PhotoGalleryFragment.java
@@ -1,19 +1,32 @@
 package com.art.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PhotoGalleryFragment extends Fragment {
 
+    private static final String TAG = "PhotoGalleryFragment";
+
     private RecyclerView mPhotoRecyclerView;
+    private List<GalleryItem> mItems = new ArrayList<>();
+    private boolean loading = true;
+    private int pastVisibleItems, visibleItemCount, totalItemCount;
+    private GridLayoutManager mLayoutManager;
+    private String numberPage = "1";
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -23,6 +36,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute(numberPage);
     }
 
     @Nullable
@@ -32,7 +46,96 @@ public class PhotoGalleryFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
 
         mPhotoRecyclerView = view.findViewById(R.id.photo_recycler_view);
-        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        mLayoutManager = new GridLayoutManager(getActivity(), 3);
+        mPhotoRecyclerView.setLayoutManager(mLayoutManager);
+        mPhotoRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                visibleItemCount = mLayoutManager.getChildCount();
+                totalItemCount = mLayoutManager.getItemCount();
+                pastVisibleItems = mLayoutManager.findLastVisibleItemPosition();
+
+
+                    if ((visibleItemCount + pastVisibleItems) >= totalItemCount){
+
+                        int countPage = Integer.parseInt(numberPage);
+                        countPage++;
+                        numberPage = String.valueOf(countPage);
+                        Log.i(TAG, "Numder page: " + numberPage);
+
+                        //new FetchItemsTask().execute(numberPage);
+                    }
+
+            }
+        });
+        setupAdapter();
         return view;
+    }
+
+    private void setupAdapter() {
+        if (isAdded()) {
+            mPhotoRecyclerView.setAdapter(new PhotoAdpter(mItems));
+        }
+    }
+
+    private class FetchItemsTask extends AsyncTask<String, Void, List<GalleryItem>> {
+
+        @Override
+        protected List<GalleryItem> doInBackground(String... page) {
+
+            return new FlickrFetchr().fetchItems(page[0]);
+
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> items) {
+            mItems = items;
+            setupAdapter();
+        }
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTitleTextView;
+        public PhotoHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mTitleTextView = (TextView) itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem item) {
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+    private class PhotoAdpter extends RecyclerView.Adapter<PhotoHolder> {
+
+        private List<GalleryItem> mGalleryItems;
+
+        public PhotoAdpter(List<GalleryItem> galleryItems) {
+            mGalleryItems = galleryItems;
+        }
+
+        @NonNull
+        @Override
+        public PhotoHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            TextView textView = new TextView(getActivity());
+            return new PhotoHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PhotoHolder photoHolder, int i) {
+            GalleryItem galleryItem = mGalleryItems.get(i);
+            photoHolder.bindGalleryItem(galleryItem);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGalleryItems.size();
+        }
     }
 }
